00b986d39fa7bdf88f78bcf9856b5cabed992516
cbethune <cbethune@uncharted.software>
2019-06-02 22:07:37 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='DistilFuzzyJoin',
-    version='0.2.0',
+    version='0.2.1',
     description='Placeholder fuzzy join',
     packages=['fuzzyjoin'],
     keywords=['d3m_primitive'],
