0eff0970b24374634e0caf356191ad7c7bc6775c
cbethune <cbethune@uncharted.software>
2019-06-03 11:19:48 -0400

    moves pandas version forward

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=['fuzzyjoin'],
     keywords=['d3m_primitive'],
     install_requires=[
-        'pandas >= 0.23.2',
+        'pandas >= 0.23.4',
         'frozendict>=1.2',
         'fuzzywuzzy>=0.17.0',
         'python-Levenshtein>=0.12.0',
